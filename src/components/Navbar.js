16023ef9b968d5edd78c835d57e821cd60cecf5e
Hakan <hakantas212@gmail.com>
2019-11-18 10:16:42 +0100

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import cart from "../cart.svg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
 
@@ -8,33 +9,48 @@ function Navbar() {
   return (
     <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       <Link to="/">
-        <img src={logo} alt="store" className="navbar-brand" />
+        <img src={logo} alt="logo" className="navbar-brand" />
       </Link>
-      <ul className="navbar-nav align-items-center">
+      <ul className="navbar-nav mx-auto align-items-center text-center">
+        <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
         <li className="nav-item ml-5">
           <Link to="/" className="nav-link">
             Products
           </Link>
         </li>
+        <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">
+            About
+          </Link>
+        </li>
       </ul>
-      <Link to="/cart" className="ml-auto">
-        <ButtonContainer>
-          <span className="mr-2">
-            <i className="fas fa-cart-plus"></i>
-          </span>
-          My Cart
-        </ButtonContainer>
+      <Link to="/cart" className="nav-item ml-5">
+        <span className="mr-2">
+          <img src={cart} alt="logo" className="navbar-brand" />
+        </span>
       </Link>
     </NavWrapper>
   );
 }
 
 const NavWrapper = styled.nav`
-  background: var(--mainBlue);
+  width: 100%;
+  top: 0;
+  left: 0;
+  box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.1);
+  -moz-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.1);
+  -webkit-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.1);
   .nav-link {
-    color: var(--mainWhite) !important;
+    color: var(--mainDark) !important;
     font-size: 1.3rem;
-    text-transform: capitalize;
+  }
+  .navbar-brand {
+    width: 50px;
+    height: 50px;
   }
 `;
 
