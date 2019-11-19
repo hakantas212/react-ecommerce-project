547e179c3953eb278848d0af9ece13bec8bafc2e
Hakan <hakantas212@gmail.com>
2019-11-19 21:55:19 +0100

    products updates

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -6,8 +6,8 @@ const images = [
   "https://images.unsplash.com/photo-1479219136056-56bb6495a005?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80",
   "https://images.unsplash.com/photo-1479064555552-3ef4979f8908?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
   "https://images.unsplash.com/photo-1512415073221-64c70eee25bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
-  "https://images.unsplash.com/photo-1520088441246-4e0e8037fed1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80",
-  "https://images.unsplash.com/photo-1550640964-4775934de4af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+  "https://images.unsplash.com/photo-1519423788125-49e7a13c35a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+  "https://images.unsplash.com/photo-1473188588951-666fce8e7c68?ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80"
 ];
 
 function ImageGallery() {
